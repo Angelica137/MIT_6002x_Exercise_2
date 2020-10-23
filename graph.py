76f04a95e8403b430231571871a03f0f66873fd7
Angelica <Angelica137@users.noreply.github.com>
2020-10-23 17:05:40 +0100

    add edges

---
+++ graph.py
@@ -0,0 +1,95 @@
+class Node(object):
+    def __init__(self, name):
+        """Assumes name is a string"""
+        self.name = name
+
+    def getName(self):
+        return self.name
+
+    def __str__(self):
+        return self.name
+
+
+class Edge(object):
+    def __init__(self, src, dest):
+        """Assumes src and dest are nodes"""
+        self.src = src
+        self.dest = dest
+
+    def getSource(self):
+        return self.src
+
+    def getDestination(self):
+        return self.dest
+
+    def __str__(self):
+        return self.src.getName() + '->(' + str(self.weight) + ')'\
+            + self.dest.getname()
+
+
+class Digraph(object):
+    # nodes is a list of the nodes in the graph
+    # edges is a dict mapping each node to a list of its children
+    def __init__(self):
+        self.nodes = []
+        self.edges = {}
+
+    def addNode(self, node):
+        if node in self.nodes:
+            raise ValueError('Duplicate node')
+        else:
+            self.nodes.append(node)
+            self.edges[node] = []
+
+    def addEdge(self, edge):
+        src = edge.getSource()
+        dest = edge.getDestination()
+        if not (src in self.nodes and dest in self.nodes):
+            raise ValueError('Node not in graph')
+        self.edges[src].append(dest)
+
+    def childrenOf(self, node):
+        return self.edges[node]
+
+    def hasNode(self, node):
+        return node in self.nodes
+
+    def __str__(self):
+        result = ""
+        for src in self.nodes:
+            for dest in self.edges[src]:
+                result = result + src.getName() + '->'\
+                    + dest.getName() + '\n'
+        return result[:-1]  # omit final newline
+
+
+class Graph(Digraph):
+    def addEdge(self, edge):
+        Digraph.addEdge(self, edge)
+        rev = Edge(edge.getDestination(), edge.getSource())
+        Digraph.addEdge(self, rev)
+
+
+nodes = []
+nodes.append(Node("ABC"))  # nodes[0]
+nodes.append(Node("ACB"))  # nodes[1]
+nodes.append(Node("BAC"))  # nodes[2]
+nodes.append(Node("BCA"))  # nodes[3]
+nodes.append(Node("CAB"))  # nodes[4]
+nodes.append(Node("CBA"))  # nodes[5]
+
+
+g = Graph()
+for n in nodes:
+    g.addNode(n)
+
+
+g.addEdge(Edge(nodes[0], nodes[1]))
+g.addEdge(Edge(nodes[0], nodes[2]))
+g.addEdge(Edge(nodes[1], nodes[4]))
+g.addEdge(Edge(nodes[2], nodes[3]))
+g.addEdge(Edge(nodes[3], nodes[5]))
+g.addEdge(Edge(nodes[4], nodes[5]))
+
+
+print(g)
